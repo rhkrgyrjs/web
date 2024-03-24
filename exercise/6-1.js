99481bba6cb6cb3bb6b400fbb82014f4db36daef
rhkrgyrjsl <rhkrgyrjsl@naver.com>
2024-03-24 19:51:02 +0900

    240324-1

---
+++ exercise/6-1.js
@@ -0,0 +1,13 @@
+var div = document.getElementById("div");
+
+div.onmouseover = 
+function() 
+{
+    this.style.backgroundColor = "yellow";
+};
+
+div.onmouseout = 
+function() 
+{
+    this.style.backgroundColor = "white";
+};
